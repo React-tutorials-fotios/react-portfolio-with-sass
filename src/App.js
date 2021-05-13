bbb726e460c11fb20bddc4edf74960e1c93f1575
Fotios Tsakiris <footios76@gmail.com>
2021-05-13 14:02:12 +0300

    added all pages - initial structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.scss";
 import NavBar from "./Components/NavBar";
-import HomePage from "./pages/HomePage";
+import { Switch, Route } from "react-router-dom";
 
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import PortfoliosPage from "./pages/PortfoliosPage";
+import BlogsPage from "./pages/BlogsPage";
+import ContactPage from "./pages/ContactPage";
 function App() {
   return (
     <div className="App">
@@ -10,7 +15,23 @@ function App() {
       </div>
       <div className="main-content">
         <div className="content">
-          <HomePage />
+          <Switch>
+            <Route path="/" exact>
+              <HomePage />
+            </Route>
+            <Route path="/about" exact>
+              <AboutPage />
+            </Route>
+            <Route path="/portfolios" exact>
+              <PortfoliosPage />
+            </Route>
+            <Route path="/blogs" exact>
+              <BlogsPage />
+            </Route>
+            <Route path="/contact" exact>
+              <ContactPage />
+            </Route>
+          </Switch>
         </div>
       </div>
     </div>
